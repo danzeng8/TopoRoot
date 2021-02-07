bd4336d7490a625c8d3107c5316906aa6984fa40
Dan Zeng <danzeng@wustl.edu>
2021-02-07 12:04:41 -0600

    updated possible inputs for pipeline

--- root_pipeline_batch.py
+++ root_pipeline_batch.py
@@ -6,7 +6,7 @@ import csv
 def main(argv):
    inputfile = ''
    outputfile = ''
-   downsample = 1
+   downsample = 6
    try:
       opts, args = getopt.getopt(argv,"hi:o:v:d:",[""])
    except getopt.GetoptError:
@@ -26,11 +26,16 @@ def main(argv):
    print('Output directory is ', outputfile)
    print('Downsampling rate is ', downsample)
    for filename in os.listdir(inputfile):
-   	if filename.endswith(".raw"):
-   		print("Processing ", inputfile+filename)
-   		datFile = inputfile+filename[0:-3] + "dat"
-   		outputStr = outputfile + filename[0:-4]
-   		os.system("python root_pipeline.py -i " + inputfile+filename + " -x " + datFile + " -o " + outputStr + " -d " + str(downsample) + " -b " + outputfile + "traits.csv")
+      if not filename.endswith(".dat") and not filename.endswith(".raw"):
+         outputStr = outputfile + filename[0:-4]
+         os.system("python root_pipeline.py -i " + inputfile+filename + " -o " + outputStr + " -d " + str(downsample) + " -b " + outputfile + "traits.csv")
+      if filename.endswith(".raw"):
+         print("Processing ", inputfile+filename)
+         datFile = inputfile+filename[0:-3] + "dat"
+         outputStr = outputfile + filename[0:-4]
+         os.system("python root_pipeline.py -i " + inputfile+filename + " -x " + datFile + " -o " + outputStr + " -d " + str(downsample) + " -b " + outputfile + "traits.csv")
+
+
 
 
 if __name__ == "__main__":
