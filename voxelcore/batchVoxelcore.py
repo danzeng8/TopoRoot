c54dcc0d203e6cdb366ed8613e497a1d844ed41e
Dan Zeng <danzeng@wustl.edu>
2021-07-30 16:40:11 -0500

    voxelcore

--- voxelcore/batchVoxelcore.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-for i in range(len(sys.argv)-1):
-	volName = sys.argv[i+1]
-	volName = str(volName[:-4])
-	os.system("C:/Users/danzeng/Desktop/voxelcore/voxelcore -md=vol2ma " + sys.argv[i+1] + " " + volName + ".ply -tt=0.001 -fullOrPruned=2")
-	os.system("C:/Users/danzeng/Desktop/voxelcore/voxelcore -md=vol2mesh " + sys.argv[i+1] + " " + volName + " -onlyBndryVts=False")
--- voxelcore/tmp.py
+++
@@ -1,1 +0,0 @@
-import subprocess; cmd='voroUtil.exe -md=topo -meshToCheck=\"d:\\3d models\\big\\Hydralisk_obj\\Hydralisk_1.off\" -isMan=false'; subprocess.run(cmd) 
